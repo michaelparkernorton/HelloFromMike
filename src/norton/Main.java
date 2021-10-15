e264c2724fb58b05699f5bda0db2e9c330096aa1
Mike <michaelparkernorton.va@gmail.com>
2021-10-15 14:35:21 -0400

    Adding a welcome message

--- src/norton/Main.java
+++ src/norton/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.print("Hello, my name is Mike.");
-        System.out.print("\nHello, my name is Tiempo.");
+        System.out.println("Hello, my name is Mike.");
+        System.out.println("Hello, my name is Tiempo.");
+        System.out.println("Welcome Tiempo!");
     }
 }
